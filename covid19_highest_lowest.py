3d68a616808a98f42a6808ec7ad1e512f409fd77
Peter Chin <43484383+map-nerd-peter@users.noreply.github.com>
2020-05-15 01:24:40 -0700

    Clean up variables, use DataParameter max/min enum

--- covid19_highest_lowest.py
+++ covid19_highest_lowest.py
@@ -82,19 +82,18 @@ class Covid19Data:
         except:
             return 'Invalid Date'
 
-    def get_plot_data(self, maximum):
+    def get_plot_data(self, DataParameter):
 
         #returns a named tuple with dates and confirmed daily_cases
         plot_data = collections.namedtuple('plot_data',['dates', 'daily_cases', 'label', 'alternate_label']) 
 
-        max_value_col_location = -1
-
+        max_value_location = -1
         #Reserved for alternate peak value
-        alt_max_value_col_location = -1
+        alt_max_value_location = -1
 
-        min_value_col_location = -1
+        min_value_location = -1
         #For alternate min value
-        alt_min_value_col_location = -1
+        alt_min_value_location = -1
 
         #Only used if there is an alternate max or alternate min value in the dataset
 
@@ -103,15 +102,8 @@ class Covid19Data:
         elif self.data_location == DataLocation.country_region:
             data = self.csv_row_data.iloc[0,2:]
 
-        print('Daily case numbers being calculated: ')
+        print('Daily case numbers in the dataset: ')
         print(data.diff())
-
-
-        print('Maximum:')
-        print(data.diff().max())
-
-        print('Getting the MEAN value of daily cases for Entire Series:')
-        print(data.diff().mean())
 
         '''print('Rolling mean method of cells')
         for date_index, value in data.diff().rolling(5, center=True).mean().items():
@@ -132,15 +124,13 @@ class Covid19Data:
         for date_index, value in rolling_data.items():
 
             if value == max_rolling_value:
-                #print('Max date and roll value are %s %f' %(date_index, value))
-
                 #Get the date for max case value for that date
                 max_rolling_value_location = list(self.csv_row_data).index(date_index)
                 max_date = date_index
                 print('Rolling Maximum column loc and dates: %s %s' %(max_rolling_value_location, max_date))
                 break
 
-        #Take data subset for the max value and its epidemilogical wave
+        #Take data subset for the max value and its epidemiological wave
         print('Data subset containing max value:')        
         print(self.csv_row_data.iloc[0,max_rolling_value_location-7:max_rolling_value_location+7].diff())
 
@@ -148,77 +138,64 @@ class Covid19Data:
         max_value = self.csv_row_data.iloc[0,max_rolling_value_location-7:max_rolling_value_location+7].diff().max()
 
         # Get values for max value location, maximum date, alternate max value location, and alternate max date.
-        max_value_location, max_date, alt_max_value_location, alt_max_date  = self.get_values_and_dates(max_value, alt_max_value, DataParameter.maximum.name, data)
+        max_value_location, max_date, alt_max_value_location, alt_max_date  = self.get_locations_and_dates(max_value, alt_max_value, DataParameter.maximum.name, data)
 
         print('Testing to see if we have 2 different maximum values %d %d' %(max_value, alt_max_value))
 
         #Comparing numpy.int64 max values,
-        if max_value == alt_max_value:
+        if max_value == alt_max_value and max_date == alt_max_date:
             print('No alternate maximum detected!!')
             plot_data.alternate_label = None
         else:
             plot_data.alternate_label = '\nAlternate maximum value detected on %s: %d new cases' %(self.get_date_label(alt_max_date), alt_max_value)
 
-        #if and else here
-
-        if maximum:
-            daily_cases= self.csv_row_data.iloc[0,max_value_location-4:max_value_location+4].diff()
+        if DataParameter.maximum:
+            daily_cases = self.csv_row_data.iloc[0,max_value_location-4:max_value_location+4].diff()
             daily_cases = daily_cases.iloc[1:8]
             label = 'Maximum Value for Daily Cases on ' + self.get_date_label(max_date)
 
         # Get minimum or trough value after a peak value. 
-        elif maximum == False:
+        elif DataParameter.minimum:
 
             #Find the min value in the rolling data
             min_rolling_data = self.csv_row_data.iloc[0,max_value_location:].diff().rolling(5, center=True).mean().round(3)
             
-            min_data = self.csv_row_data.iloc[0,max_value_location:].diff()
+            min_data = self.csv_row_data.iloc[0,max_value_location:]
 
             min_rolling_value = min_rolling_data.min()
             print('Min rolling value %.2f' %min_rolling_value)
 
-            #There may be alternate max value that is a result of quirks or daily revision of data reporting, not the max value we want.
-            min_value = min_data.min()
-            print('Minimum Value')
-            print(min_value)
-
-            alt_min_value = data.diff().max()
-            print('Alt max value')
-            print(alt_max_value)
-
-            #Get column location of rolling value first
+            #Get column location of minimum value
             for date_index, value in min_rolling_data.items():
 
                 if value == min_rolling_value:
                     min_rolling_value_location = list(self.csv_row_data).index(date_index)
                     min_date = date_index
-                    print('Rolling Maximum column loc and dates: %s %s' %(min_rolling_value_location, min_date))
+                    print('Rolling Minimum column loc and dates: %s %s' %(min_rolling_value_location, min_date))
                     break
 
-            #Take data subset for the min value and its epidemilogical wave
-            print('Data subset containing max value:')        
-            print(self.csv_row_data.iloc[0,min_rolling_value_location-7:min_rolling_value_location+7].diff())
+            #The likely Minimum value of the epidemioliogical wave
+            min_value = self.csv_row_data.iloc[0,min_rolling_value_location-7:min_rolling_value_location+7].diff().min()
 
-            #This is likely the true min within the epidemioliogical wave
-            alt_min_value = self.csv_row_data.iloc[0,min_rolling_value_location-7:min_rolling_value_location+7].diff().min()
+            #There may be alternate minimum value that is a result of quirks/daily revision of data reporting, not the minimum we want.
+            alt_min_value = min_data.diff().min()
+            
+            # Get values for min value location, min date, alt minimum value and alt min date
+            min_value_location, min_date, alt_min_value_location, alt_min_date  = self.get_locations_and_dates(min_value, alt_min_value, DataParameter.minimum.name, min_data)
 
-            #Possibly a different minimum value here
-            min_value = min_data.min()
+            print('Checking the Min value and alt minimum value: %d %d' %(min_value, alt_min_value))
 
-            #Identify Min Value and Possible Alternate Min value
-            min_value_location, min_date, alt_min_value_location, alt_min_date  = self.get_values_and_dates(min_value, alt_min_value, DataParameter.minimum.name, data)
-
-            if min_value == alt_min_value:
+            if min_value == alt_min_value and min_date == alt_min_date:
                 print('No alternate minimum detected!!')
                 plot_data.alternate_label = None
             else:
-                plot_data.alternate_label = '\nAlternate minimum value detected on %s: %d new cases' %(self.get_date_label(alt_min_date), alt_min_value)
+                plot_data.alternate_label = '\nAlternate Minimum value detected on %s: %d new cases' %(self.get_date_label(alt_min_date), alt_min_value)
 
-            daily_cases= self.csv_row_data.iloc[0,min_value_location-4:min_value_location+4].diff()
+            daily_cases = self.csv_row_data.iloc[0,min_value_location-4:min_value_location+4].diff()
             daily_cases = daily_cases.iloc[1:8]
             label = 'Minimum Value for Daily Cases on ' + self.get_date_label(min_date)
 
-        #Get corresponding day for each case va
+        #Get corresponding day for each case value
         dates= [day for day, value in daily_cases.iteritems()]
 
         plot_data.dates = dates
@@ -227,16 +204,15 @@ class Covid19Data:
 
         return plot_data
 
-    def get_values_and_dates(self, value, alternate_value, DataParameter, data_series):
+    def get_locations_and_dates(self, value, alternate_value, DataParameter, data_series):
 
         #parameters: value and alternate_value, DataParameter maximum.name or minimum.name, Pandas data Series,
         #Depending on DataParameter maximum or minimum, 
         #it returns 4 variables: max_value_location, max_date, alt_max_value_location, alt_max_date or 
         #min_value_location, min_date, alt_min_value_location, alt_min_date
-
-        #Identify Max Value and Possible Alternate max value
+    
         for date_index, val in data_series.diff().items():
-
+        
             if val == value:
                 value_location = list(self.csv_row_data).index(date_index)
                 date = date_index
@@ -251,7 +227,6 @@ class Covid19Data:
 
     def plot_bar_chart(self, plot_data):
 
-        #Check that dates (x-values) and daily case numbers are valid 
         if plot_data.dates is not None and plot_data.daily_cases is not None:
             plt.bar(plot_data.dates, height = plot_data.daily_cases) 
             plt.xticks(plot_data.dates, [self.get_date_label(d) for d in plot_data.dates])
@@ -264,7 +239,6 @@ class Covid19Data:
             else:
                 plt.title('COVID-19 Epidemic in %s\n%s' %(self.location, plot_data.label))
             plt.show()
-
 
 def main():
 
@@ -310,10 +284,10 @@ def main():
 
         #Plot the maximum or high value of the data
         if args.type == 'highest':
-            results = covid_data.get_plot_data(maximum = True)
+            results = covid_data.get_plot_data(DataParameter.maximum)
         #Plot the minimum or low value of the data
         elif args.type == 'lowest':
-            results = covid_data.get_plot_data(maximum = False)
+            results = covid_data.get_plot_data(DataParameter.minimum)
 
         #url https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv
         covid_data.plot_bar_chart(results)
